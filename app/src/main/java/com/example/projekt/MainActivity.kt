ad4f03819eaedd7ac90c2469ab415a86a898d63e
nicknilssonmoren <nicknilssonmoren@gmail.com>
2021-10-12 16:02:17 +0200

    Todo list version, gonna change direction and do it with a different tutorial instead

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -4,32 +4,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvUserId: TextView
-    private lateinit var tvEmailId: TextView
     private lateinit var btnLogout: Button
+    private lateinit var todoAdapter: TodoAdapter
+    private lateinit var rvTodoItems: RecyclerView
+    private lateinit var btnAddTodo: Button
+    private lateinit var btnDeleteDoneTodo: Button
+    private lateinit var etTodoTitle: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvUserId = findViewById(R.id.tv_user_id)
-        tvEmailId = findViewById(R.id.tv_email_id)
-        btnLogout = findViewById(R.id.btn_logout)
-
-
-        val userId = intent.getStringExtra("user_id")
-        val emailId = intent.getStringExtra("email_id")
-
-        tvUserId.text = "User ID :: $userId"
-        tvEmailId.text = "Email ID :: $emailId"
+        btnLogout = findViewById(R.id.btnLogout)
 
         btnLogout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+
+        btnDeleteDoneTodo.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/projekt/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.projekt
+
+data class Todo (
+    val title: String,
+    var isChecked: Boolean = false
+)
